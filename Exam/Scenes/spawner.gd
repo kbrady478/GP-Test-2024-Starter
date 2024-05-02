cfa9ae71ebe284e3f9141d8a74c50b8696199a74
kbrady478 <c23789389@mytudublin.ie>
2024-05-02 12:25:12 +0100

    Slider linked + working

--- Exam/Scenes/spawner.gd
+++ Exam/Scenes/spawner.gd
@@ -1,11 +1,24 @@
 extends Control
 
+var amount_To_Spawn: int
+
+
+@onready var h_slider = $CanvasLayer/HSlider
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	amount_To_Spawn = h_slider.value
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_texture_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_h_slider_value_changed(value):
+	amount_To_Spawn = int(value)
+	print(value)

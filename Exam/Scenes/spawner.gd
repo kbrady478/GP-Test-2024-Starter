88f3d1d832bdbb5b3c270f5ad9bcbadd48682a98
kbrady478 <c23789389@mytudublin.ie>
2024-05-02 13:22:04 +0100

    Fixed falling, basic spawning

--- Exam/Scenes/spawner.gd
+++ Exam/Scenes/spawner.gd
@@ -1,23 +1,32 @@
 extends Control
 
 var amount_To_Spawn: int
+var spawn_Array = []
 
 @onready var slider_num = $"CanvasLayer/Panel/Panel/Slider Num"
 @onready var h_slider = $CanvasLayer/Panel/Panel/HSlider
+
+
+
+@onready var spawning = $CanvasLayer/Panel/Panel/Spawning
+@onready var obj = preload("res://Scenes/falling_Object.tscn")
+var obj_Instance 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	amount_To_Spawn = h_slider.value
 	slider_num.text = str(h_slider.value)
-
+	obj_Instance = obj.instantiate()
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 
 func _on_texture_button_pressed():
-	pass # Replace with function body.
+	spawning.add_child(obj_Instance)
 
 
 func _on_h_slider_value_changed(value):
